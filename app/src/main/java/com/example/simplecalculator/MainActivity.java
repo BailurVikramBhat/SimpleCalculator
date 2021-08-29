9ae3d4377e118871b0200b655d4018ffff3506da
BailurVikramBhat <bailurvikrambhat@gmail.com>
2021-08-29 18:07:54 +0530

    added double functionality -closes #4

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Stack;
 import java.util.regex.Pattern;
 
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView answer;
 
-    boolean equalButtonPressed, divisionByZero, hadNeg, OneOperator, hasOpenParanthesis;
+    boolean equalButtonPressed, divisionByZero, hadNeg, OneOperator, hasOpenParanthesis, doubleRem;
 
 
 
@@ -61,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 findViewById(R.id.divide),
                 findViewById(R.id.modulus),
                 findViewById(R.id.clear),
+                findViewById(R.id.buttonPeriod),
                 findViewById(R.id.button00),
                 findViewById(R.id.delete),
                 findViewById(R.id.equalTo)
@@ -78,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hadNeg = false;
         OneOperator = false;
         hasOpenParanthesis = false;
+        doubleRem = false;
 
 
 
@@ -177,24 +181,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             updateAnswer.append("00");
         }
 
-        /*else if(idGotFrommView == R.id.buttonPeriod) {
+        else if(idGotFrommView == R.id.buttonPeriod) {
             if(equalButtonPressed) {
                 updateAnswer.setText(answer.getText().toString());
                 answer.setText("");
-                equalButtonPressed = false;
             }
-
-            if(!hasOpenParanthesis) {
-                updateAnswer.append("(");
-                hasOpenParanthesis = true;
-            }
-            else {
-                updateAnswer.append(")");
-                hasOpenParanthesis = false;
-            }
+            equalButtonPressed = false;
+            updateAnswer.append(".");
 
 
-        }*/
+        }
         else if(idGotFrommView == R.id.add) {
             if(equalButtonPressed) {
                 updateAnswer.setText(answer.getText().toString());
@@ -241,6 +237,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             updateAnswer.setText("");
         }
         else if(idGotFrommView == R.id.delete) {
+            hasOpenParanthesis = false;
 
             if(answer.getText().toString().equals("Can't divide by Zero") || answer.getText().toString().equals("invalid")) {
                 updateAnswer.setText("");
@@ -263,12 +260,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
         else if(idGotFrommView == R.id.modulus) {
+
+
+
+
             if(equalButtonPressed) {
                 updateAnswer.setText(answer.getText().toString());
-                answer.setText("");
+                answer.setText("%");
+                equalButtonPressed = false;
             }
             equalButtonPressed = false;
+            answer.setText("");
             updateAnswer.append("%");
+
+//
+//            if(!hasOpenParanthesis) {
+//                updateAnswer.append("(");
+//                hasOpenParanthesis = true;
+//            }
+//            else {
+//                updateAnswer.append(")");
+//                hasOpenParanthesis = false;
+//            }
         }
         else if(idGotFrommView == R.id.equalTo) {
             equalButtonPressed = true;
@@ -286,21 +299,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 expression = expression.replaceAll("\u00F7", "/");
                 updateAnswer.setText("");
                 if(isValid(expression)) {
-                    int ans = evaluate(expression);
+//                    int ans = evaluate(expression);
+                    Double doubleans = eval(expression);
                     if(divisionByZero) {
                        answer.setTextColor(Color.parseColor("#FF0000"));
                        answer.setText("Can't divide by Zero");
                        divisionByZero = false;
                     }
-                    else if(hadNeg && OneOperator){
-                        answer.setTextColor(Color.parseColor("#ffff4444"));
-                        answer.setText("-"+Integer.toString(ans));
-                        hadNeg = false;
-                        OneOperator = false;
+                    else if(doubleRem) {
+                        answer.setTextColor(Color.parseColor("#FF0000"));
+                        answer.setText("Can't find % for doubles");
+                        doubleRem = false;
+
                     }
                     else {
                         answer.setTextColor(Color.parseColor("#ffff4444"));
-                        answer.setText(Integer.toString(ans));
+//                        answer.setText(Integer.toString(ans));
+                        if(doubleans == Math.floor(doubleans)) {
+                            answer.setText(String.format(Locale.US, "%.0f", doubleans));
+                        }
+                        else {
+                            answer.setText(Double.toString(doubleans));
+                        }
                     }
 
 
@@ -325,7 +345,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         char[] tokens = s.toCharArray();
         int ans = 0;
         String storage = "+-*/%";
-        if(!Pattern.matches("-?\\d+([-+*%/]-?\\d+)*", s)) {
+        if(!Pattern.matches("-?\\d+([-+*%/.()]-?\\d+)*", s)) {
             return false;
         }
         if(s.length() == 1 && storage.contains(Character.toString(tokens[0]))) return false;
@@ -347,10 +367,125 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+    int order(char operator) {
+        if(operator == '+' || operator == '-') {
+            return 1;
+        }
+        if(operator == '*'  || operator == '/') {
+            return 2;
+        }
+        return 0;
+    }
 
+    double calculation(double op2, char op, double op1) {
+        if(op == '-') return op1 - op2;  //last out - first out eg. "3-2", 3 will be first in. then 2 in
+        if(op == '+') return op1 + op2;
+        if(op == '*') return op1 * op2;
+        if(op == '%') {
+            if(op1 == Math.floor(op1) && op2 == Math.floor(op2)) {
 
+                return op1%op2;
+            }
+            else doubleRem = true;
+        }
+        if(op == '/') {
 
+            if(op2 == 0) {
+                divisionByZero = true;
+            }
+            else return op1 / op2;
+        }
+        return 0;
+    }
 
+    double eval(String expression) {
+        int len = expression.length();
+        char[] tokens = expression.toCharArray();
+        System.out.println(Arrays.toString(tokens));
+        Stack<Double> operands = new Stack<>();
+        Stack<Character> operator = new Stack<>();
+        for(int i=0; i<len; i++) {
+            System.out.println("New iteration");
+            if(tokens[i] == ' ') {
+                continue;
+            }
+            else if(Character.isDigit(tokens[i])) {
+                StringBuilder ans = new StringBuilder();
+                while(i<len && (Character.isDigit(tokens[i]))) {
+                    System.out.println("Evaluating: "+tokens[i]+" i value is "+i);
+                    ans.append(tokens[i]);
+                    i++;
+                }
+                if(i<len && tokens[i] == '.') {
+                    i++;
+                    System.out.println("Found dot");
+                    ans.append(".");
+                    while(i<len && Character.isDigit(tokens[i])) {
+                        System.out.println("Evaluating: "+tokens[i]+" i value is "+i);
+                        ans.append(tokens[i]);
+                        i++;
+                    }
+                }
+                System.out.println("ans: "+ans.toString());
+                operands.push(Double.parseDouble(ans.toString()));
+                System.out.println(operands.peek());
+                i--;
+            }
+            else if(tokens[i] == '(') {
+                operator.push(tokens[i]);
+            }
+            else if(tokens[i] == ')') {
+                while(!operator.empty() && operator.peek()!='(') {
+                    double a = operands.pop();
+                    double b = operands.pop();
+                    char c = operator.pop();
+                    operands.push(calculation(a, c, b));
+                }
+                operator.pop();
+            }
+            else {
+                String minus = "";
+                if(tokens[i] == '-') {
+                    if(!operator.empty()) {
+                        if(operator.peek() == '(') {
+                            operands.push(0.0);
+                        }
+                        else if(i==0) {
+                            // first num is negative.
+                            double b = operands.pop();
+                            operands.push(Double.parseDouble("-"+b));
+                        }
+                    }
+
+                }
+                while(!operator.empty() && order(operator.peek())>=order(tokens[i])) {
+                    double a = operands.pop();
+                    double b;
+                    if(operands.empty()) {
+                        b=0;
+                    }
+                    else {
+                        b = operands.pop();
+                    }
+                    char c = operator.pop();
+                    operands.push(calculation(a, c, b));
+                }
+                operator.push(tokens[i]);
+            }
+
+        }
+        while(!operator.empty()) {
+            double a = operands.pop();
+            double b;
+            if(operands.empty()) {
+                b=0;
+            }
+            else b = operands.pop();
+            char c = operator.pop();
+            operands.push(calculation(a, c, b));
+        }
+        return operands.peek();
+    }
 
 
 
